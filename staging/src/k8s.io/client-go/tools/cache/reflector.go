32747c209db2ddd40de491260535b6b91456d1a8
feeli <feeli@tencent.com>
2022-04-26 20:41:38 +0800

    set event log level

--- staging/src/k8s.io/client-go/tools/cache/reflector.go
+++ staging/src/k8s.io/client-go/tools/cache/reflector.go
@@ -335,7 +335,7 @@ loop:
 				continue
 			}
 			newResourceVersion := meta.GetResourceVersion()
-			fmt.Printf("<<<<<<------event = %v ----->>>>>>>>", event)
+			klog.V(4).Infof("<<<<<<------event = %v ----->>>>>>>>", event)
 			switch event.Type {
 			case watch.Added:
 				err := r.store.Add(event.Object)
